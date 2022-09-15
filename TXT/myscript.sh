4ba2714c1d65fe18425779f912f0e4e3a23e910b
Airel Camilo Khairan <87690657+airelcamilo@users.noreply.github.com>
2022-09-15 22:33:59 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="60914D29C01C81F1"
+REC2="1F8DB7BC7E58C0B6"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
